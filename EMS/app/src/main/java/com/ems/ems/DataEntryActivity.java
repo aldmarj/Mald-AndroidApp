d7108bceb20c2b24c847631d4cece557517869f5
Aldmar <aldmar.joubert@students.plymouth.ac.uk>
2017-11-04 18:11:39 +0000

    Form controls added for data entry

--- EMS/app/src/main/java/com/ems/ems/DataEntryActivity.java
+++ EMS/app/src/main/java/com/ems/ems/DataEntryActivity.java
@@ -1,13 +1,35 @@
 package com.ems.ems;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class DataEntryActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class DataEntryActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_entry);
+    }
+
+
+
+    public void cancelEntry(View view) {
+    }
+
+    public void saveEntry(View view) {
+    }
+
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public void showDatePickerDialog(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
